2b7b4a48a86e8009930726c6589dea6c0f35b5fe
joaopamarall <joaoamaral56@hotmail.com>
2023-12-20 20:29:13 -0300

    feat: Criar solução da questão 1

--- src/app/questoes/1/page.jsx
+++ src/app/questoes/1/page.jsx
@@ -1,12 +1,21 @@
+'use client'
+
+import './page.css'
 export default function Questao1() {
-  return (
-    <>
-      <h1>Questão 1</h1>
-      <p>
-        Crie um botão vermelho com as bordas arredondadas que exiba "Clique-me!"
-        como texto. Ao clicar no botão, um alerta deve ser disparado avisando
-        que o botão foi clicado.
-      </p>
-    </>
-  );
+	const handleClick = () => {
+		alert('O botão foi clicado!');
+	  };
+  	return (
+		<>
+			<h1>Questão 1</h1>
+			<p>
+				Crie um botão vermelho com as bordas arredondadas que exiba "Clique-me!"
+				como texto. Ao clicar no botão, um alerta deve ser disparado avisando
+				que o botão foi clicado.
+			</p>
+			<div className='redBtn'>
+				<button onClick={handleClick}>Clique-me!</button>
+			</div>
+		</>
+  )
 }
