68d18cc0393f39b60e6c57ca2e6eb39abbeb4d77
joaopamarall <joaoamaral56@hotmail.com>
2023-12-20 20:34:43 -0300

    feat: Criar solução da questão 3

--- src/app/questoes/3/page.jsx
+++ src/app/questoes/3/page.jsx
@@ -1,6 +1,11 @@
+'use client'
+import './page.css'
 const numerosInteiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function Questao3() {
+
+  const numerosPares = numerosInteiros.filter((element) => element % 2 === 0);
+  const soma = numerosPares.reduce((acumulador, valorAtual) => acumulador + valorAtual, 0);
   return (
     <>
       <h1>Questão 3</h1>
@@ -8,7 +13,7 @@ export default function Questao3() {
         Escreva uma função JavaScript que recebe uma matriz de números inteiros
         e retorna a soma de todos os números pares. Exiba a soma na tela.
       </p>
-      <p>Soma:</p>
+      <p className="soma">Soma: {soma}</p>
     </>
   );
 }
